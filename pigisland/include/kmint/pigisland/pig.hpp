d3676966e6538cfe8a25738a6320166b7e2829e8
Stefan Willems <sgpwille@avans.nl>
2018-12-12 12:08:09 +0100

    reformated

--- pigisland/include/kmint/pigisland/pig.hpp
+++ pigisland/include/kmint/pigisland/pig.hpp
@@ -4,22 +4,23 @@
 #include "kmint/play.hpp"
 
 namespace kmint {
-namespace pigisland {
+	namespace pigisland {
 
-class pig : public play::free_roaming_actor {
-public:
-  explicit pig(math::vector2d location);
-  const ui::drawable &drawable() const override { return drawable_; }
-  void move(math::vector2d delta) { location(location() + delta); }
-  void act(delta_time dt) override;
-  bool perceptive() const override { return true; }
-  scalar range_of_perception() const override { return 30.0f; }
+		class pig : public play::free_roaming_actor {
+		public:
+			explicit pig(math::vector2d location);
+			const ui::drawable &drawable() const override { return drawable_; }
+			void move(math::vector2d delta) { location(location() + delta); }
+			void act(delta_time dt) override;
+			bool perceptive() const override { return true; }
+			scalar range_of_perception() const override { return 30.0f; }
 
-private:
-  play::image_drawable drawable_;
-};
+		private:
 
-} // namespace pigisland
+			play::image_drawable drawable_;
+		};
+
+	} // namespace pigisland
 } // namespace kmint
 
 #endif /* KMINT_PIGISLAND_PIG_HPP */
--- pigisland/include/kmint/pigisland/shark.hpp
+++ pigisland/include/kmint/pigisland/shark.hpp
@@ -5,23 +5,23 @@
 #include "kmint/play.hpp"
 #include "kmint/primitives.hpp"
 namespace kmint {
-namespace pigisland {
-class shark : public play::map_bound_actor {
-public:
-  shark(kmint::map::map_graph &g);
-  ui::drawable const &drawable() const override { return drawable_; }
-  bool incorporeal() const override { return false; }
-  scalar radius() const override { return 16.0; }
-  void act(delta_time dt) override;
+	namespace pigisland {
+		class shark : public play::map_bound_actor {
+		public:
+			shark(kmint::map::map_graph &g);
+			ui::drawable const &drawable() const override { return drawable_; }
+			bool incorporeal() const override { return false; }
+			scalar radius() const override { return 16.0; }
+			void act(delta_time dt) override;
 
-private:
-  play::image_drawable drawable_;
-  map::map_graph *map_;
-  map::map_node const *resting_place_;
-  delta_time t_since_move_{};
-};
+		private:
+			play::image_drawable drawable_;
+			map::map_graph *map_;
+			map::map_node const *resting_place_;
+			delta_time t_since_move_{};
+		};
 
-} // namespace pigisland
+	} // namespace pigisland
 } // namespace kmint
 
 #endif /* KMINT_PIGISLAND_SHARK_HPP */
